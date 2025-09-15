680655582c5582fecc46a0e8d34222a55ce64c94
root <root@ip-10-0-1-86.ec2.internal>
2025-09-15 12:47:50 +0000

    Testing Jenkins CI/CD pipeline

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Docker CI/CD World!"
+    return "Co..coo.cooo.coooo coolie power house"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
